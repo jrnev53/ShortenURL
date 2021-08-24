6fd51d93685bef32a614964f903cbf3b74e3b1a2
John Neville <jrnev53@gmail.com>
2021-08-24 14:01:32 -0400

    Initialized Spring project

---
+++ src/main/java/com/jakcan/shortservice/ShortServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jakcan.shortservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakcan/shortservice/ShortServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakcan.shortservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
